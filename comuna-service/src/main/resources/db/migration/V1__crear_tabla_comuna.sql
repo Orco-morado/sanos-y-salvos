f4577cb4151a19e6a0f28fd59daeb63907c88c97
vicente guajardo <vi.guajardoc@duocuc.cl>
2026-05-25 19:25:38 -0400

    ARREGLE EL CAGASO MIO

--- comuna-service/src/main/resources/db/migration/V1__crear_tabla_comuna.sql
+++
@@ -1,7 +0,0 @@
-CREATE TABLE comuna (
-    id_comuna int(3) NOT NULL AUTO_INCREMENT,
-    nombre_comuna varchar(50) NOT NULL,
-    cod_postal int(11) NOT NULL,
-    activo tinyint(1) NOT NULL,
-    PRIMARY KEY (id_comuna)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
--- reporte_avistamiento-service/src/main/resources/db/migration/V1__crear_tabla_avistamiento.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE reporte_avistamiento (
-    id_avistamiento int(3) NOT NULL AUTO_INCREMENT,
-    descripcion varchar(50) NOT NULL,
-    direccion_avistamiento varchar(20) NOT NULL,
-    persona_avistamiento varchar(40) NOT NULL,
-    fecha_avistamiento date NOT NULL,
-    estado_avistamiento tinyint(1) NOT NULL,
-    PRIMARY KEY (id_avistamiento)
-);
