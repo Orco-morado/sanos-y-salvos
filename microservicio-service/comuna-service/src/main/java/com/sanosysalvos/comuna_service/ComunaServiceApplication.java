20670aca0ee0cd5e6496420b9ffbd0870fbe95f1
Your Name <you@example.com>
2026-06-15 11:41:22 -0400

    primeros deo micros con eureka

--- microservicio-service/comuna-service/src/main/java/com/sanosysalvos/comuna_service/ComunaServiceApplication.java
+++ microservicio-service/comuna-service/src/main/java/com/sanosysalvos/comuna_service/ComunaServiceApplication.java
@@ -2,8 +2,10 @@ package com.sanosysalvos.comuna_service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class ComunaServiceApplication {
 
 	public static void main(String[] args) {
